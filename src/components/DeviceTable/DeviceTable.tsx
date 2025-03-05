bc3903e1d2f8ead1be41ebf3eed1bcff29cb1fa1
Kashish Gupta <kashishguptatech@gmail.com>
2025-03-05 11:05:27 +0530

    global selected checkbox done

--- src/components/DeviceTable/DeviceTable.tsx
+++ src/components/DeviceTable/DeviceTable.tsx
@@ -8,39 +8,6 @@ import GlobalSelectCheckbox from "../GlobalSelectCheckbox";
 import DeviceTableHeader from "./DeviceTableHeader";
 import DeviceTableRow from "./DeviceItem";
 
-const files = [
-    {
-        name: "smss.exe",
-        device: "Stark",
-        path: "\\Device\\HarddiskVolume2\\Windows\\System32\\smss.exe",
-        status: "Scheduled",
-    },
-    {
-        name: "netsh.exe",
-        device: "Targaryen",
-        path: "\\Device\\HarddiskVolume2\\Windows\\System32\\netsh.exe",
-        status: "Available",
-    },
-    {
-        name: "uxtheme.dll",
-        device: "Lannister",
-        path: "\\Device\\HarddiskVolume1\\Windows\\System32\\uxtheme.dll",
-        status: "Available",
-    },
-    {
-        name: "cryptbase.dll",
-        device: "Martell",
-        path: "\\Device\\HarddiskVolume1\\Windows\\System32\\cryptbase.dll",
-        status: "Scheduled",
-    },
-    {
-        name: "7za.exe",
-        device: "Baratheon",
-        path: "\\Device\\HarddiskVolume1\\temp\\7za.exe",
-        status: "Scheduled",
-    },
-];
-
 const DeviceTable = () => {
     const [devices, setDevices] = useState<Device[]>(defaultDeviceData);
 
--- src/components/GlobalSelectCheckbox.tsx
+++ src/components/GlobalSelectCheckbox.tsx
@@ -1,4 +1,5 @@
 import { Device } from "@/types/device.type";
+import { useEffect, useState } from "react";
 
 interface GlobalSelectCheckboxProps {
     devices: Array<Pick<Device, "id" | "isSelected">>;
@@ -10,10 +11,28 @@ const GlobalSelectCheckbox = ({
     setDeSelectAll,
     setSelectAll,
 }: GlobalSelectCheckboxProps) => {
+    const numberOfSelectedDevices = devices.filter(
+        (d) => d.isSelected === true
+    ).length;
+
+    const [isChecked, setIsChecked] = useState<boolean>(numberOfSelectedDevices === devices.length ? true : false);
+
+    useEffect(() => {
+        setIsChecked(numberOfSelectedDevices === devices.length ? true : false)
+    }, [numberOfSelectedDevices]);
+
     return (
         <div>
-            <input type="checkbox" className="mr-2" />
-            <span className="text-gray-700 font-medium">None Selected</span>
+            <input
+                type="checkbox"
+                className="mr-2"
+                checked={isChecked}
+                onChange={(e) => e.target.checked ? setSelectAll() : setDeSelectAll()}
+            />
+            <span className="text-gray-700 font-medium">
+                {numberOfSelectedDevices > 0 ? numberOfSelectedDevices : "None"}{" "}
+                Selected
+            </span>
         </div>
     );
 };
